6cfcd0eadebbb5272417d6ed36f0a2f2a37b784d
Sunil Raiyani <sunilraiyani4793@gmail.com>
2015-10-09 20:58:41 -0700

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,6 @@
-$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchRun.sh
-showTimes.sh > /home/hduser/nfs/Big-Bench/results4datanodes/out1g.txt
-hdfs dfsadmin -report > /home/hduser/nfs/Big-Bench/results4datanodes/report1g.txt
-showErrors.sh > /home/hduser/nfs/Big-Bench/results4datanodes/err1g.txt
-
-$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchRun.sh 5
-showTimes.sh > /home/hduser/nfs/Big-Bench/results4datanodes/out5g.txt
-hdfs dfsadmin -report > /home/hduser/nfs/Big-Bench/results4datanodes/report5g.txt
-showErrors.sh > /home/hduser/nfs/Big-Bench/results4datanodes/err5g.txt
-
-$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchRun.sh 10
-showTimes.sh > /home/hduser/nfs/Big-Bench/results4datanodes/out10g.txt
-hdfs dfsadmin -report > /home/hduser/nfs/Big-Bench/results4datanodes/report10g.txt
-showErrors.sh > /home/hduser/nfs/Big-Bench/results4datanodes/err10g.txt
-
-$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchRun.sh 40
-showTimes.sh > /home/hduser/nfs/Big-Bench/results4datanodes/out40g.txt
-hdfs dfsadmin -report > /home/hduser/nfs/Big-Bench/results4datanodes/report40g.txt
-showErrors.sh > /home/hduser/nfs/Big-Bench/results4datanodes/err40g.txt
+$BIG_BENCH_BASH_SCRIPT_DIR/bigBenchRun.sh 100
+showTimes.sh > /home/hduser/nfs/Big-Bench/results4datanodes/out100g.txt
+hdfs dfsadmin -report > /home/hduser/nfs/Big-Bench/results4datanodes/report100g.txt
+showErrors.sh > /home/hduser/nfs/Big-Bench/results4datanodes/err100g.txt
 
 
